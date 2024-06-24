c2a92e819a6c2e06c6b0cd4e660e114f4007231f
SiriusRak <diaryrakitrniela@gmail.com>
2024-06-24 15:47:21 +0300

    modif

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -7,10 +7,10 @@ void    biby()
     char *line, *val;
     char  valiny[250];
 
-    fd = open("dic/biby.txt", O_RDONLY);
-    fd2 = open("dic/valiny.txt", O_RDONLY);
+    fd = open("../dic/biby.txt", O_RDONLY);
+    fd2 = open("../dic/valiny.txt", O_RDONLY);
     printf("IZA ARY AHO?\n");
-    while(line = get_next_line(fd))
+    while((line = get_next_line(fd)))
     {
         while (line[0] == i + '0')
         {
@@ -18,8 +18,8 @@ void    biby()
             val = get_next_line(fd2);
             val += 2;
             printf("Izaho dia %s", line);
-            scanf("%s", &valiny);
-            for (int j = 0; j < strlen(valiny); j++) {
+            scanf("%s", valiny);
+            for (size_t j = 0; j < strlen(valiny); j++) {
                 valiny[j] = tolower(valiny[j]);
             }
             if (strstr(val, valiny))
